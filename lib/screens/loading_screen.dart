e86f473713437103dbd46552bb3e3e31602cb823
Ishan Srivastava <ishanmay8@gmail.com>
2020-11-06 22:47:21 +0530

    getting data form api completed

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/screens/location_screen.dart';
+const String ApiKey='56e4a1e403dad4979e3a61e4e8ff3805';
+
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,16 +13,32 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double longitude;
+  double latitude;
+  @override
+  void initState() {
+    super.initState();
+   getLocationData();
+  }
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    longitude=location.longitude;
+    latitude=location.latitude;
+    var weatherData=NetworkingHelper('api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$ApiKey');
+
+    Navigator.push(context, MaterialPageRoute(builder: (context)=>LocationScreen()));
+
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
+        
+       child: SpinKitCirclen(color: Colors.red,size: 50,),
       ),
     );
   }
